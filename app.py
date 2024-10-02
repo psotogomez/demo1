865de2b7b0cffc3cc9bddd4de0db8ea602464585
psotogomez <125215250+psotogomez@users.noreply.github.com>
2024-10-01 21:14:42 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://github.com/psotogomez/demo1/blob/main/independencia_geocoded_filtered.csv?raw=True', sep=';')  
+
+app = Dash()
+
+app.layout = [
+    html.H1(children='Independencia Chile', style={'textAlign':'center'}),
+    dcc.Dropdown(['MIGRANTE', 'SEXO', 'MAYOR'], 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+]
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    return px.scatter(df, x='lat', y='lon',hover_data=['SEXO', 'new_dir', 'RUN'], color=value)
+
+if __name__ == '__main__':
+    app.run(debug=True)
